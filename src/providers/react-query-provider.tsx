46d7eb2890808918b0b2c2f71f0eb79d28dae083
het-tale <ettalebyhasnaa99@gmail.com>
2024-07-31 19:03:56 +0100

    fix: resolve react query error

--- src/providers/react-query-provider.tsx
+++ src/providers/react-query-provider.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React from "react";
 
 /**
  * ReactQueryProvider is a higher-order component that wraps the application or a part of it
@@ -12,12 +13,12 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
  * @returns {React.ReactNode} The QueryClientProvider component wrapping the child components.
  */
 
-const queryClient = new QueryClient();
 export default function ReactQueryProvider({
 	children
 }: {
 	children: React.ReactNode;
 }) {
+	const [queryClient] = React.useState(() => new QueryClient());
 	return (
 		<QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
 	);
